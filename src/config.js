64890c92b4950c034c7138b0d3d084e243aca366
jobal <jbaldwije@gmail.com>
2022-08-24 23:30:40 -0400

    change

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports = {
   //   process.env.Database_URL ||
   //   "ostgres://bmkdrszi:EocyHusuL9OTHF9rHOR2M6iH03e7mIII@ruby.db.elephantsql.com:5432/bmkdrszi",
   Database_URL:
-    process.env.Database_URL2 ||
+    process.env.Database_URL ||
     "postgres://wuxmyupo:xGM_krJk0pKaOfNE-KSldK4ctX5UnCTM@suleiman.db.elephantsql.com/wuxmyupo",
 };
