e489acaa99998875513e22a47db5bc12b42f68d2
jobal <jbaldwije@gmail.com>
2022-08-24 23:41:29 -0400

    id

--- src/devotions/devotions-router.js
+++ src/devotions/devotions-router.js
@@ -14,33 +14,33 @@ DevotionsRouter.route("/").get((req, res, next) => {
     .catch(next);
 });
 
-// DevotionsRouter.route("/:devotionId")
-//   .all((req, res, next) => {
-//     DevotionsService.getById(req.app.get("db"), req.params.devotionId)
-//       .then((devotion) => {
-//         if (!devotion) {
-//           return res.status(404).json({
-//             error: { message: `Devotion doesn't exist` },
-//           });
-//         }
-//         res.devotion = devotion;
-//         next();
-//       })
-//       .catch(next);
-//   })
+DevotionsRouter.route("/:devotionId")
+  .all((req, res, next) => {
+    DevotionsService.getById(req.app.get("db"), req.params.devotionId)
+      .then((devotion) => {
+        if (!devotion) {
+          return res.status(404).json({
+            error: { message: `Devotion doesn't exist` },
+          });
+        }
+        res.devotion = devotion;
+        next();
+      })
+      .catch(next);
+  })
 
-//   .get((req, res, next) => {
-//     const knexInstance = req.app.get("db");
-//     DevotionsService.getById(knexInstance, req.params.devotionId)
-//       .then((devotion) => {
-//         if (!devotion) {
-//           return res.status(404).json({
-//             error: { message: `Devotion doesn't exist` },
-//           });
-//         }
-//         res.json(devotion);
-//       })
-//       .catch(next);
-//   });
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    DevotionsService.getById(knexInstance, req.params.devotionId)
+      .then((devotion) => {
+        if (!devotion) {
+          return res.status(404).json({
+            error: { message: `Devotion doesn't exist` },
+          });
+        }
+        res.json(devotion);
+      })
+      .catch(next);
+  });
 
 module.exports = DevotionsRouter;
